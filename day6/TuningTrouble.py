88214f2f8fc6031b2d71aa7137826fecd0bba08a
Jakob Svemar <bgm@qlik.com>
2022-12-06 07:49:37 +0100

    optimise day6 solution
    len(set) faster than going through the string to find duplicates

--- day6/TuningTrouble.py
+++ day6/TuningTrouble.py
@@ -1,13 +1,6 @@
-def checkUnique(str):
-  for i in range(len(str)):
-    for j in range(i + 1,len(str)):
-      if(str[i] == str[j]):
-        return False
-  return True
-
 def findSequence(type, input, length):
     for i in range(0, len(input) - length):
-        if checkUnique(input[i:i + length]):
+        if len(set(input[i:i + length])) == length:
             print('first ' + type + ' sequence after: ' + str(i + length))
             return
 
