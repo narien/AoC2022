d2df49a779c7d8d242c708e3920c42c962667d11
Jakob Svemar <bgm@qlik.com>
2022-12-08 08:15:37 +0100

    add day8 part1 solution

--- day8/TreetopTreeHouse.py
+++ day8/TreetopTreeHouse.py
@@ -1,39 +1,43 @@
 def buildGrid(lines):
     grid = []
+    currLine = 0
     for line in lines:
-        grid.append(line)
+        grid.append([])
+        for char in line:
+            grid[currLine].append(int(char))
+        currLine += 1
     return grid
 
 def treeIsVisible(x, y, grid):
-    myHeight = grid[y][x]
+    myHeight = int(grid[y][x])
     if x == 0 or x == len(grid[0]) - 1:
         return True
     if y == 0 or y == len(grid) - 1:
         return True
 
-    for i in range(0, x):
-        print (i)
-        # if myHeight <= grid[y][i]:
-        #     return False
+    lh = set(grid[y][0:x])
+    if max(lh) < myHeight:
+        return True
 
+    rh = set(grid[y][x+1:len(grid[y])])
+    if max(rh) < myHeight:
+        return True
 
-
-    for i in range(0, x):
-        if myHeight <= grid [y][i]:
-            return False
-    for i in range(x+1, len(grid[0])):
-        if myHeight < grid [y][i]:
-            return False
+    tv = set()
     for i in range(0, y):
-        if myHeight < grid [i][x]:
-            return False
-    for i in range(y+1, len(grid)):
-        if myHeight < grid [i][x]:
-            return False
-    
-    return True
+        tv.add(grid[i][x])
+    if max(tv) < myHeight:
+        return True
 
+    bv = set()
+    for i in range(y+1, len(grid)):
+        bv.add(grid[i][x])
+    if max(bv) < myHeight:
+        return True
     
+    return False
+
+
 
 def countVisibleTrees(grid):
     total = 0
