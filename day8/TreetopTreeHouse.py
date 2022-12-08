9813988b02bf1c19d82f89031d05f3c782eca6bd
Jakob Svemar <bgm@qlik.com>
2022-12-08 09:08:17 +0100

    add day8 part2 solution

--- day8/TreetopTreeHouse.py
+++ day8/TreetopTreeHouse.py
@@ -48,6 +48,39 @@ def countVisibleTrees(grid):
 
     return total
 
+def calcScenicScore(grid):
+    maxScore = 0
+
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid[y])):
+            left = 0
+            right = 0
+            up = 0
+            down = 0
+            for i in range(x - 1, -1, -1):
+                left += 1
+                if grid[y][i] >= grid[y][x]:
+                    break
+
+            for i in range(x + 1, len(grid[y])):
+                right += 1
+                if grid[y][i] >= grid[y][x]:
+                    break
+
+            for i in range(y - 1, -1, -1):
+                up += 1
+                if grid[i][x] >= grid[y][x]:
+                    break
+
+            for i in range(y + 1, len(grid)):
+                down += 1
+                if grid[i][x] >= grid[y][x]:
+                    break
+
+            total = left*right*up*down
+            maxScore = total if total > maxScore else maxScore
+    return maxScore
+
 if __name__ == '__main__':
     with open('day8/input.txt') as f:
         lines = [line.rstrip('\n') for line in f]
@@ -55,3 +88,4 @@ if __name__ == '__main__':
     grid = buildGrid(lines)
 
     print('visible trees: ' + str(countVisibleTrees(grid)))
+    print('highest scenic score: ' + str(calcScenicScore(grid)))
