84cefe758e65050896d654bd616a8f634675f626
Jakob Svemar <bgm@qlik.com>
2022-12-12 10:29:07 +0100

    add day 12 part2 solution

--- day12/HillClimbingAlgorithm.py
+++ day12/HillClimbingAlgorithm.py
@@ -1,3 +1,5 @@
+import sys
+
 def buildMap(lines):
     tMap = []
     for line in lines:
@@ -5,10 +7,16 @@ def buildMap(lines):
     return tMap
 
 def findStart(tMap):
+    suitableStarts = []
     for y in range(len(tMap)):
         for x in range(len(tMap[y])):
             if(tMap[y][x] == 'S'):
-                return x, y
+                suitableStarts.append((x,y))
+    for y in range(len(tMap)):
+        for x in range(len(tMap[y])):
+            if(tMap[y][x] == 'a'):
+                suitableStarts.append((x,y))
+    return suitableStarts
 
 def foundTop(x, y, tMap):
     if tMap[y][x] == 'E':
@@ -42,11 +50,20 @@ def traverse(tMap, x, y):
         if foundTop(x, y, tMap):
             return stepCount
         addNewSteps(x, y, stepCount + 1, queue, tMap, haveVisited)
+    return sys.maxsize
 
 if __name__ == '__main__':
     with open('day12/input.txt') as f:
         lines = [line.strip() for line in f]
     tMap = buildMap(lines)
-    x, y = findStart(tMap)
-    tMap[y][x] = 'a'
-    print('shortest path is: ' + str(traverse(tMap, x, y)))
+    starts = findStart(tMap)
+    sX, sY = starts[0]
+    tMap[sY][sX] = 'a'
+
+    counts = set()
+    for x, y in starts:
+        count = traverse(tMap, x, y)
+        if x == sX and y == sY:
+            print('shortest path from S is: ' + str(count))
+        counts.add(count)
+    print('shortest path from any a is: ' + str(min(counts)))
