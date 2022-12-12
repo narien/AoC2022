6363d07d3c82619be6f1e75e83e2c0a89f7925e8
Jakob Svemar <bgm@qlik.com>
2022-12-12 10:07:11 +0100

    save day 12 progress

--- day12/HillClimbingAlgorithm.py
+++ day12/HillClimbingAlgorithm.py
@@ -1,6 +1,3 @@
-import sys
-haveVisited = set()
-
 def buildMap(lines):
     tMap = []
     for line in lines:
@@ -13,42 +10,38 @@ def findStart(tMap):
             if(tMap[y][x] == 'S'):
                 return x, y
 
-def traverse(tMap, x, y, stepCount):
-    haveVisited.add((x,y))
+def foundTop(x, y, tMap):
     if tMap[y][x] == 'E':
-        return stepCount
-    stepCount += 1
+        return True
+    return False
 
-    minCount = 999999999
+def addNewSteps(x, y, stepCount, queue, tMap, haveVisited):
     if x < len(tMap[0]) - 1 and (x+1, y) not in haveVisited:
         if abs(ord(tMap[y][x]) - ord(tMap[y][x+1])) <= 1 or tMap[y][x] in ['y', 'z'] and tMap[y][x+1] == 'E':
-            count = traverse(tMap, x+1, y, stepCount)
-            if count < minCount:
-                minCount = count
-    
+            queue.append((x+1, y, stepCount))
+            haveVisited.add((x+1, y))
     if x > 0 and (x-1, y) not in haveVisited:
         if abs(ord(tMap[y][x]) - ord(tMap[y][x-1])) <= 1 or tMap[y][x] in ['y', 'z'] and tMap[y][x-1] == 'E':
-            count = traverse(tMap, x-1, y, stepCount)
-            if count < minCount:
-                minCount = count
-
+            queue.append((x-1, y, stepCount))
+            haveVisited.add((x-1, y))
     if y < len(tMap) - 1 and (x, y+1) not in haveVisited:
         if abs(ord(tMap[y][x]) - ord(tMap[y+1][x])) <= 1 or tMap[y][x] in ['y', 'z'] and tMap[y+1][x] == 'E':
-            count = traverse(tMap, x, y+1, stepCount)
-            if count < minCount:
-                minCount = count
-    
+            queue.append((x, y+1, stepCount))
+            haveVisited.add((x, y+1))
     if y > 0 and (x, y-1) not in haveVisited:
         if abs(ord(tMap[y][x]) - ord(tMap[y-1][x])) <= 1 or tMap[y][x] in ['y', 'z'] and tMap[y-1][x] == 'E':
-            count = traverse(tMap, x, y-1, stepCount)
-            if count < minCount:
-                minCount = count
-    return minCount
-    
+            queue.append((x, y-1, stepCount))
+            haveVisited.add((x, y-1))
 
-
-
-
+def traverse(tMap, x, y):
+    haveVisited = set()
+    haveVisited.add((x, y))
+    queue = [(x, y, 0)]
+    while len(queue):
+        x, y, stepCount = queue.pop(0)
+        if foundTop(x, y, tMap):
+            return stepCount
+        addNewSteps(x, y, stepCount + 1, queue, tMap, haveVisited)
 
 if __name__ == '__main__':
     with open('day12/input.txt') as f:
@@ -56,5 +49,4 @@ if __name__ == '__main__':
     tMap = buildMap(lines)
     x, y = findStart(tMap)
     tMap[y][x] = 'a'
-    sys.setrecursionlimit(6000)
-    print('shortest path is: ' + str(traverse(tMap, x, y, 0)))
+    print('shortest path is: ' + str(traverse(tMap, x, y)))
