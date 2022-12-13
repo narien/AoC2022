b22e56dca6b2f4c7e53e12f02afd2f208e56032d
Jakob Svemar <bgm@qlik.com>
2022-12-13 08:29:51 +0100

    add day 13 part1 solution

--- day13/DistressSignal.py
+++ day13/DistressSignal.py
@@ -2,20 +2,47 @@ import copy
 import json
 
 def myEval(p1, p2):
+    if type(p1) != list and type(p2) != list:
+        if p1 == p2:
+            return 0
+        elif p1 < p2:
+            return -1
+        else:
+            return 1
+
     if type(p1) == list and type(p2) != list:
         p2 = [p2]
     if type(p1) != list and type(p2) == list:
         p1 = [p1]
 
-    if type(p1) == list and type(p2) == list:
-        myEval(p1[0])
-    if p1 < p2:
-        return True
-    return False
+    for i in range(len(p1)):
+        if i < len(p2):
+            res = myEval(p1[i], p2[i])
+            if type(res) is bool:
+                return res
+            if res == 0:
+                continue
+            elif res == -1:
+                return True
+            elif res == 1:
+                return False
+            else:
+                return False
+        else:
+            return False
+    return 0 if len(p1) == len(p2) else True
 
 def evalPairs(pairs):
+    currIndex = 0
+    total = 0
     for p in pairs:
-        eval(p[0], p[1])
+        currIndex += 1
+        res = myEval(p[0], p[1])
+        if type(res) is bool and res:
+            total += currIndex
+    print('part 1 sum: ' + str(total))
+
+
 if __name__ == '__main__':
     with open('day13/input.txt') as f:
         lines = [line.strip() for line in f]
