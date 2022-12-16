1a6ba0f50ad70d211323c3853cbbb4427d5e94a6
Jakob Svemar <jakob_svemar@hotmail.com>
2022-12-16 07:05:21 +0100

    rename variable

--- day16/ProboscideaVolcanium.py
+++ day16/ProboscideaVolcanium.py
@@ -1,13 +1,13 @@
 def parseInput(lines):
-    nodeGraph = dict()
+    valveMap = dict()
     for line in lines:
         line = line.split()
         routes = [valve[0:-1] if len(valve) == 3 else valve for valve in line[9:]]
-        nodeGraph[line[1]] = (line[4][5:-1], routes)
-    return nodeGraph
+        valveMap[line[1]] = (line[4][5:-1], routes)
+    return valveMap
     
 if __name__ == '__main__':
     with open('day16/input.txt') as f:
         lines = [line.strip() for line in f]
-    nodeGraph = parseInput(lines)
-    
+    valveMap = parseInput(lines)
+
